b23d750724a86ef972205a627ad90c6d0ace7704
Kubra6270 <kubratuten864@gmail.com>
2025-06-12 23:52:15 +0300

    YurticiKargo.cs

---
+++ YurticiKargo.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KargoTakip
+{
+   public class YurticiKargo :Gonderi
+    {
+        public string Il { get; set; }
+        public string Ilce { get; set; }
+
+        public YurticiKargo(string gonderenAd, string gonderenAdres, string aliciAd, string aliciAdres, double agirlik, string il, string ilce)
+            : base(gonderenAd, gonderenAdres, aliciAd, aliciAdres, agirlik)
+        {
+            Il = il;
+            Ilce = ilce;
+        }
+
+        public override string TakipBilgisi()
+        {
+            return $"[YURTİÇİ] {TakipNumarasi} | {GonderenAd} → {AliciAd} | {Il}/{Ilce} | {DurumMetni()} | {Agirlik}kg | {TeslimatUcreti():C}";
+        }
+
+        public override double TeslimatUcreti()
+        {
+            return Agirlik * 20; // Yurtiçi kg başına 20TL olsun
+        }
+
+        private string DurumMetni()
+        {
+            switch (Durum)
+            {
+                case KargoDurum.Hazirlaniyor:
+                    return "Hazırlanıyor";
+                case KargoDurum.Yolda:
+                    return "Yolda";
+                case KargoDurum.TeslimEdildi:
+                    return "Teslim Edildi";
+                default:
+                    return "Bilinmiyor";
+            }
+        }
+    }
+}
